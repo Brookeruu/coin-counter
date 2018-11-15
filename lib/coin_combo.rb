082717950dffd8254b0c679621b4bd6108dd3138
brooke perry and Daniel Bennett <brooke@>
2018-11-15 10:19:52 -0800

    write function and specs

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,15 +1,29 @@
 # class Coin
 #   attr_accessor(:amount, :quarters, :dimes, :nickles, :pennies)
 
-  def coin_counter(number)
-    amount = number
-    quarters = 0
-    dimes = 0
-    nickles = 0
-    pennies = 0
+def coin_counter(number)
+  amount = number
+  quarters = 0
+  dimes = 0
+  nickels = 0
+  pennies = 0
 
-    if amount >= 25
-      quarter = amount / 25.floor
-    end
+  if amount >= 0
+    quarters = amount / 25.floor
+    amount = amount - quarters * 25
   end
-# end
+  if amount >=10
+    dimes = amount / 10.floor
+    amount = amount - dimes * 10
+  end
+  if amount >=5
+    nickels = amount / 5.floor
+    amount = amount - nickels * 5
+  end
+  if amount >=1
+    pennies = amount
+  end
+  coin_array = [quarters, dimes, nickels, pennies]
+  # quarters = coin_array[0]
+  # dimes = coin_array[1]
+end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -4,6 +4,36 @@ require 'coin_combo'
 
 describe ('#coin_combo') do
   it('display coin amount') do
-    expect(coin_counter(52)).to(eq(2))
+    expect(coin_counter(51)).to(eq([2, 0, 0, 1]))
   end
+
+  it('display the number of quarters, dimes, nickles, and pennies') do
+    expect(coin_counter(84)).to(eq([3, 0, 1, 4]))
+  end
+
+  it('25 will display 1 at [0]') do
+    expect(coin_counter(25)).to(eq([1, 0, 0, 0]))
+  end
+
+  it('10 will display 1 at [1]') do
+    expect(coin_counter(10)).to(eq([0, 1, 0, 0]))
+  end
+
+  it('5 will display 1 at [2]') do
+    expect(coin_counter(5)).to(eq([0, 0, 1, 0]))
+  end
+
+  it('1 will display 1 at [3]') do
+    expect(coin_counter(1)).to(eq([0, 0, 0, 1]))
+  end
+
+  # it('display the number of quarters, dimes, nickles, and pennies') do
+  #   expect(coin_counter(25)).to(eq(1))
+  # end
+
+  # #it('display the number of quarters, dimes, nickles, and pennies') do
+  #   expect(coin_counter(10)).to(eq(1))
+  # end
+
+
 end
